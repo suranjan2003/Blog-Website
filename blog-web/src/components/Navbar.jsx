9d5b3da324809f5bf8b9a0e4795ec32dac9fa109
suranjan2003 <karmakarsuranjan12@gmail.com>
2024-10-06 12:06:06 +0530

    toggle menu done

--- blog-web/src/components/Navbar.jsx
+++ blog-web/src/components/Navbar.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react'; // Import useState for mobile menu state management
-import { Link } from 'react-router-dom';
-import { FaFacebook, FaGlobe, FaSquareGithub, FaBars } from "react-icons/fa6";
+import { Link, useLocation } from 'react-router-dom';
+import { FaFacebook, FaGlobe, FaSquareGithub, FaBars, FaXmark } from "react-icons/fa6";
 
 const Navbar = () => {
     const navItems = [
@@ -11,6 +11,7 @@ const Navbar = () => {
     ];
 
     const [isMobileMenuOpen, setMobileMenuOpen] = useState(false); // State for managing mobile menu visibility
+    const location = useLocation();
 
     const toggleMobileMenu = () => {
         setMobileMenuOpen(!isMobileMenuOpen); // Toggle mobile menu state
@@ -18,41 +19,44 @@ const Navbar = () => {
 
     return (
         <header className='bg-gradient-to-r from-blue-500 via-purple-500 to-red-700 fixed right-0 left-0 top-0'>
-            <nav className="px-4 py-4 max-w-7xl mx-auto flex justify-between items-center">
+            <nav className="px-4 py-2 max-w-7xl mx-auto flex justify-between items-center">
                 {/* Logo */}
                 <a href="/" className="classname-logo text-2xl font-bold text-white">
-                    Optima<span className='text-yellow-400'>2025</span>
+                    Design<span className='text-yellow-400'>SK</span>
                 </a>
 
                 {/* Mobile menu button */}
-                <div className="md:hidden">
+                <div className="md:hidden text-white">
                     <button 
                         onClick={toggleMobileMenu} // Add onClick to toggle the mobile menu
-                        className='cursor-pointer text-white hover:text-yellow-300'
+                        className='cursor-pointer hover:text-yellow-300'
                     >
-                        <FaBars />
+                        {isMobileMenuOpen ? <FaXmark /> : <FaBars />}
                     </button>
                 </div>
 
                 {/* Navigation items and icons */}
                 <div className="hidden md:flex items-center space-x-8">
-                    <ul className="flex space-x-8 text-base">
+                    <ul className="flex space-x-8 text-white text-base">
                         {navItems.map((item, index) => (
                             <li key={index}>
-                                <Link to={item.path} className="text-white hover:text-gray-300">{item.Link}</Link>
+                                <Link 
+                                to={item.path} 
+                                className={`hover:text-gray-300 ${location.pathname === item.path ? 'text-yellow-300' : ''}`} // Add active class
+                                >{item.Link}</Link>
                             </li>
                         ))}
                     </ul>
 
                     {/* Icons */}
-                    <div className='flex space-x-4 px-4'>
-                        <div className='text-white hover:text-yellow-400'>
+                    <div className='flex space-x-4 px-4 text-white'>
+                        <div className='hover:text-yellow-400'>
                             <FaFacebook />
                         </div>
-                        <div className='text-white hover:text-yellow-400'>
+                        <div className='hover:text-yellow-400'>
                             <FaGlobe />
                         </div>
-                        <div className='text-white hover:text-yellow-400'>
+                        <div className='hover:text-yellow-400'>
                             <FaSquareGithub />
                         </div>
                     </div>
@@ -68,11 +72,16 @@ const Navbar = () => {
 
             {/* Mobile Menu */}
             {isMobileMenuOpen && ( // Conditionally render the mobile menu
-                <div className="md:hidden">
-                    <ul className="flex flex-col space-y-2 bg-black bg-opacity-50 text-white p-4">
+                <div className="md:hidden text-white">
+                    <ul className="flex flex-col space-y-2 bg-black bg-opacity-50 p-4">
                         {navItems.map((item, index) => (
                             <li key={index}>
-                                <Link to={item.path} className="block text-white hover:text-yellow-300">{item.Link}</Link>
+                                <Link 
+                                to={item.path} 
+                                className={`hover:text-yellow-300 ${location.pathname === item.path ? 'text-yellow-300' : ''}`} // Add active class
+                                onClick={toggleMobileMenu}
+                                >{item.Link}
+                                </Link>
                             </li>
                         ))}
                     </ul>
